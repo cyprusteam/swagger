eefc87c43198f4eb8017bb2338895de470c774ad
Veaceslav <uveaceslav@trafficstars.com>
2015-08-14 09:03:11 +0300

    Added work with relative paths in import

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"flag"
-	"log"
+  "flag"
+  "log"
 
-	"github.com/yvasiyarov/swagger/generator"
+  "github.com/yvasiyarov/swagger/generator"
 )
 
 var apiPackage = flag.String("apiPackage", "", "The package that implements the API controllers, relative to $GOPATH/src")
@@ -12,29 +12,31 @@ var mainApiFile = flag.String("mainApiFile", "", "The file that contains the gen
 var outputFormat = flag.String("format", "go", "Output format type for the generated files: "+generator.AVAILABLE_FORMATS)
 var outputSpec = flag.String("output", "", "Output (path) for the generated file(s)")
 var controllerClass = flag.String("controllerClass", "", "Speed up parsing by specifying which receiver objects have the controller methods")
+var packageName = flag.String("packageName", "", "The package name docs file")
 
 func main() {
-	flag.Parse()
+  flag.Parse()
 
-	if *mainApiFile == "" {
-		*mainApiFile = *apiPackage + "/main.go"
-	}
+  if *mainApiFile == "" {
+    *mainApiFile = *apiPackage + "/main.go"
+  }
 
-	if *apiPackage == "" {
-		flag.PrintDefaults()
-		return
-	}
+  if *apiPackage == "" {
+    flag.PrintDefaults()
+    return
+  }
 
-	params := generator.Params{
-		ApiPackage:      *apiPackage,
-		MainApiFile:     *mainApiFile,
-		OutputFormat:    *outputFormat,
-		OutputSpec:      *outputSpec,
-		ControllerClass: *controllerClass,
-	}
+  params := generator.Params{
+    ApiPackage:      *apiPackage,
+    MainApiFile:     *mainApiFile,
+    OutputFormat:    *outputFormat,
+    OutputSpec:      *outputSpec,
+    ControllerClass: *controllerClass,
+    PackageName:     *packageName,
+  }
 
-	err := generator.Run(params)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+  err := generator.Run(params)
+  if err != nil {
+    log.Fatal(err.Error())
+  }
 }
